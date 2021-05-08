7af7035ccb5f3e42342fd443e90a589f15e01565
kethan-kumar <83391165+kethan-kumar@users.noreply.github.com>
2021-05-07 21:05:09 -0300

    Update TwitterCredentials.py

--- src/configuration/TwitterCredentials.py
+++ src/configuration/TwitterCredentials.py
@@ -1,6 +1,6 @@
 # Credential to connect to twitter
 
-consumer_key = "UicyzAEfXT48nJKJb5LPKjdBo"
-consumer_secret = "NWTgwxdsLLEQ1KX8F39isWR0QJQ9UsCW0PFMdmHNIdLektq6WD"
-access_key = "3221093575-89VAbgbudFT2hIIeLPKcBn55O8jWJM7gy9LKVHj"
-access_secret = "zcHzzSr9sGFSjB3GWKTmL28N1dBu7yzhWbQvH8sqU82Ow"
+consumer_key = "[consumer_key]"
+consumer_secret = "[consumer_secret]"
+access_key = "[access_key]"
+access_secret = "[access_secret]"
